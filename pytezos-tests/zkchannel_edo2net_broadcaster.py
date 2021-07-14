3afe8ed945e2499b0be085d5bb3b8e6823abd681
Darius Parvin <darius@berkeley.edu>
2021-07-14 01:06:38 -0700

    update pytezos tests
    update testnet tests to work with pytezos 3.2.4

--- pytezos-tests/zkchannel_edo2net_broadcaster.py
+++ pytezos-tests/zkchannel_edo2net_broadcaster.py
@@ -7,6 +7,16 @@ from pytezos import pytezos
 from pytezos import Contract
 from pytezos import ContractInterface
 import json
+class colors:
+     PURPLE = '\033[95m'
+     GREEN = '\033[92m'
+     ENDC = '\033[0m'
+
+def print_header(msg):
+    print(f"{colors.PURPLE}{msg}{colors.ENDC}")
+
+def print_green(msg):
+    print(f"{colors.GREEN}{msg}{colors.ENDC}")
 
 def read_json_file(json_file):
     f = open(json_file)
@@ -68,10 +78,11 @@ class FeeTracker:
         pprint(self.fees)
 
 def add_funding(ci, amt):
-    print("Adding funds ({})".format(amt))
-    out = ci.addFunding().with_amount(amt).inject(_async=False)
-    print("addFunding ophash: ", out['hash'])
-    return out
+    print_green(f"Adding funds ({amt})")
+    out = ci.addFunding().with_amount(amt).send(min_confirmations=1)
+    print_green(f"addFunding ophash: {out.hash()}")
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+    return opg
 
 def originate(cust_py, init_params, cust_funding, merch_funding):
     # Create initial storage for main zkchannel contract
@@ -108,14 +119,14 @@ def originate(cust_py, init_params, cust_funding, merch_funding):
     'status': 0}
 
     # Originate main zkchannel contract
-    print("Originate main zkChannel contract")
-    out = cust_py.origination(script=main_code.script(initial_storage=initial_storage)).autofill().sign().inject(_async=False)
-    print("Originate zkChannel ophash: ", out['hash'])
+    print_green("Originate main zkChannel contract")
+    out = cust_py.origination(script=main_code.script(initial_storage=initial_storage)).autofill().sign().send(min_confirmations=1)
+    print_green(f"Originate zkChannel ophash: {out.hash()}")
     # Get address of main zkchannel contract
-    opg = pytezos.shell.blocks[-20:].find_operation(out['hash'])
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
     main_id = opg['contents'][0]['metadata']['operation_result']['originated_contracts'][0]
-    print("zkChannel contract address: ", main_id)
-    return out, main_id
+    print_green(f"zkChannel contract address: {main_id}")
+    return opg, main_id
 
 def cust_close(ci, init_params, cust_close_data):
     # Form cust close storage
@@ -134,31 +145,33 @@ def cust_close(ci, init_params, cust_close_data):
         "s2": sigma2
     }
 
-    print("Broadcasting Cust Close")
-    out = ci.custClose(close_storage).inject(_async=False)
-    print("Cust Close ophash: ", out['hash'])
-    return out
+    print_green("Broadcasting Cust Close")
+    out = ci.custClose(close_storage).send(min_confirmations=1)
+    print_green(f"Cust Close ophash: {out.hash()}")
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+    return opg
 
 def merch_dispute(ci, entrypoint, rev_secret):
-    print('Broadcasting {}'.format(entrypoint))
-    cmd = 'ci.{e}(\"{r}\").inject(_async=False)'.format(e=entrypoint, r=rev_secret)
-    out = eval(cmd)
-    print("{} ophash: ".format(entrypoint), out['hash'])
-    return out
+    print_green('Broadcasting {entrypoint}')
+    cmd = 'ci.{e}(\"{r}\").send(min_confirmations=1)'.format(e=entrypoint, r=rev_secret)
+    print_green(f"{entrypoint} ophash: {out.hash()}")
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+    return opg
 
 def entrypoint_no_args(ci, entrypoint):
-    print('Broadcasting {}'.format(entrypoint))
-    cmd = 'ci.{}().inject(_async=False)'.format(entrypoint)
+    print_green(f"Broadcasting {entrypoint}")
+    cmd = 'ci.{}().send(min_confirmations=1)'.format(entrypoint)
     out = eval(cmd)
-    print("{} ophash: ".format(entrypoint), out['hash'])
-    return out
+    print_green(f"{entrypoint} ophash: {out.hash()}")
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+    return opg
 
 def scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     '''
     Scenario 1: Customer creates a single funded contract then initiates a unilateral customer closure.
     Entrypoints tested: 'addFunding', 'custClose', 'custClaim'.
     '''
-    print("Scenario 1: origination -> cust_funding -> cust_close -> cust_claim")
+    print_header("Scenario 1: origination -> cust_funding -> cust_close -> cust_claim")
 
     # A single-funded channel is originated where the customer's initial balances (in mutez) is:
     cust_funding=establish_json.get("customer_deposit")
@@ -166,8 +179,6 @@ def scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_params):
 
     out, contract_id = originate(cust_py, establish_params, cust_funding, merch_funding)
     feetracker.add_result('originate', out) # feetracker is used to track fees for benchmarking purposes 
-
-    print(f"contract id: {contract_id}")
 
     # Set the contract interfaces for cust
     cust_ci = cust_py.contract(contract_id)
@@ -189,7 +200,7 @@ def scenario2(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     Scenario 2: First the merchant initiates a unilateral closure, then the customer closes on an outdated state and is punished by the merchant.
     Entrypoints tested: 'addFunding', 'expiry', 'custClose', 'merchDispute'.
     '''
-    print("Scenario 2: origination -> cust_funding -> merch_funding -> expiry -> cust_close -> merch_dispute")
+    print_header("Scenario 2: origination -> cust_funding -> merch_funding -> expiry -> cust_close -> merch_dispute")
     # A single-funded channel is originated where the customer's initial balances (in mutez) is:
     cust_funding=establish_json.get("customer_deposit")
     merch_funding=establish_json.get("merchant_deposit")
@@ -226,7 +237,7 @@ def scenario3(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     'merchClaim' is used by the merchant to claim the total channel balance if the customer fails to respond to the 'expiry' call.
     Entrypoints tested: 'addFunding', 'reclaimFunding', 'expiry', 'merchClaim'.
     '''
-    print("Scenario 3: origination -> cust_funding -> reclaim_funding -> cust_funding -> merch_funding -> expiry -> merch_claim")
+    print_header("Scenario 3: origination -> cust_funding -> reclaim_funding -> cust_funding -> merch_funding -> expiry -> merch_claim")
 
     # Create a dual-funded channel. If the establish_json is for a single funded channel, use the customer_deposit for the merchant so that it's non-zeo
     cust_funding=establish_json.get("customer_deposit")
@@ -270,7 +281,10 @@ if __name__ == "__main__":
     # parser.add_argument("--merch_close", "-mc", help="Enter the filename (with path) to the merch_expiry.json file created by zeekoe")
     args = parser.parse_args()
 
-    print("Connecting to edo2net via: " + args.shell)
+    if args.shell:
+        pytezos = pytezos.using(shell=args.shell)
+    print("Connecting to " + args.shell)
+
     cust_acc = args.cust
     merch_acc = args.merch
     establish_json_file = args.establish
@@ -293,26 +307,26 @@ if __name__ == "__main__":
     # Activate cust and merch testnet accounts
     try:
         print("Activating cust account")
-        cust_py.activate_account().fill().sign().inject()
+        cust_py.activate_account().fill().sign().send()
     except:
         print("Cust account already activated")
 
     try:
         print("Revealing cust pubkey")
-        out = cust_py.reveal().autofill().sign().inject()
+        out = cust_py.reveal().autofill().sign().send()
     except:
         pass
     cust_pubkey = cust_py.key.public_key()
 
     try:
         print("Activating merch account")
-        merch_py.activate_account().fill().sign().inject()
+        merch_py.activate_account().fill().sign().send()
     except: 
         print("Merch account already activated")
 
     try:
         print("Revealing merch pubkey")
-        out = merch_py.reveal().autofill().sign().inject()
+        out = merch_py.reveal().autofill().sign().send()
     except:
         pass
     merch_pubkey = merch_py.key.public_key()
