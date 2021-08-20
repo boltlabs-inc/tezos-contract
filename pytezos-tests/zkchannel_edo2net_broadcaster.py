aa04d4d59e30eb4f2f2ec25d10d0e02fcf8ba03d
Darius Parvin <darius@berkeley.edu>
2021-08-20 13:03:52 -0700

    update contract and pytezos tests

--- pytezos-tests/zkchannel_edo2net_broadcaster.py
+++ pytezos-tests/zkchannel_edo2net_broadcaster.py
@@ -1,5 +1,5 @@
 # Example usage:
-# python3 zkchannel_edo2net_broadcaster.py --contract=zkchannel_contract.tz --cust=tz1S6eSPZVQzHyPF2bRKhSKZhDZZSikB3e51.json --merch=tz1VcYZwxQoyxfjhpNiRkdCUe5rzs53LMev6.json --custclose=cust_close.json --merchclose=merch_close.json 
+# python3 zkchannel_edo2net_broadcaster.py --contract=../zkchannels-contract/zkchannel_contract.tz --cust=sample_files/tz1S6eSPZVQzHyPF2bRKhSKZhDZZSikB3e51.json --merch=sample_files/tz1VcYZwxQoyxfjhpNiRkdCUe5rzs53LMev6.json --establish=sample_files/out.5f0b6efabc46808589acc4ffcfa9e9c8412cc097e45d523463da557d2c675c67.establish.json --cust-close=sample_files/out.5f0b6efabc46808589acc4ffcfa9e9c8412cc097e45d523463da557d2c675c67.close.json
 
 import argparse
 from pprint import pprint
@@ -77,10 +77,17 @@ class FeeTracker:
     def print_fees(self):
         pprint(self.fees)
 
-def add_funding(ci, amt):
+def add_cust_funding(ci, amt):
     print_green(f"Adding funds ({amt})")
-    out = ci.addFunding().with_amount(amt).send(min_confirmations=1)
-    print_green(f"addFunding ophash: {out.hash()}")
+    out = ci.addCustFunding().with_amount(amt).send(min_confirmations=1)
+    print_green(f"addCustFunding ophash: {out.hash()}")
+    opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
+    return opg
+
+def add_merch_funding(ci, amt):
+    print_green(f"Adding funds ({amt})")
+    out = ci.addMerchFunding().with_amount(amt).send(min_confirmations=1)
+    print_green(f"addMerchFunding ophash: {out.hash()}")
     opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
     return opg
 
@@ -99,21 +106,19 @@ def originate(cust_py, init_params, cust_funding, merch_funding):
     'close_flag': close_scalar_bytes,
     'context_string': "zkChannels mutual close",
     'custAddr': cust_addr, 
-    'custBal':0, 
-    'custFunding': cust_funding, 
+    'custBal':cust_funding,  
     'custPk': cust_pubkey, 
     'delayExpiry': '1970-01-01T00:00:00Z', 
     'g2':g2,
     'merchAddr': merch_addr, 
-    'merchBal': 0, 
-    'merchFunding': merch_funding, 
+    'merchBal': merch_funding, 
     'merchPk': merch_pubkey, 
-    'merchPk0': y2s[0],
-    'merchPk1': y2s[1],
-    'merchPk2': y2s[2],
-    'merchPk3': y2s[3],
-    'merchPk4': y2s[4],
-    'merchPk5': x2,
+    'y2s_0': y2s[0],
+    'y2s_1': y2s[1],
+    'y2s_2': y2s[2],
+    'y2s_3': y2s[3],
+    'y2s_4': y2s[4],
+    'x2': x2,
     'revLock': '0x00', 
     'selfDelay': 3, 
     'status': 0}
@@ -141,8 +146,8 @@ def cust_close(ci, init_params, cust_close_data):
         "custBal": cust_balance,
         "merchBal": merch_balance,
         "revLock": revocation_lock,
-        "s1": sigma1,
-        "s2": sigma2
+        "sigma1": sigma1,
+        "sigma2": sigma2
     }
 
     print_green("Broadcasting Cust Close")
@@ -166,12 +171,12 @@ def entrypoint_no_args(ci, entrypoint):
     opg = pytezos.shell.blocks[-20:].find_operation(out.hash())
     return opg
 
-def scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_params):
+def test_custclaim(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     '''
-    Scenario 1: Customer creates a single funded contract then initiates a unilateral customer closure.
-    Entrypoints tested: 'addFunding', 'custClose', 'custClaim'.
+    Customer creates a single funded contract then initiates a unilateral customer closure.
+    Entrypoints tested: 'addCustFunding', 'custClose', 'custClaim'.
     '''
-    print_header("Scenario 1: origination -> cust_funding -> cust_close -> cust_claim")
+    print_header("Scenario test_custclaim: origination -> cust_funding -> cust_close -> cust_claim")
 
     # A single-funded channel is originated where the customer's initial balances (in mutez) is:
     cust_funding=establish_json.get("customer_deposit")
@@ -183,9 +188,9 @@ def scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     # Set the contract interfaces for cust
     cust_ci = cust_py.contract(contract_id)
 
-    # add customer's balance to the contract using 'addFunding' entrypoint
-    out = add_funding(cust_ci, cust_funding)
-    feetracker.add_result('addFunding', out)
+    # add customer's balance to the contract using 'addCustFunding' entrypoint
+    out = add_cust_funding(cust_ci, cust_funding)
+    feetracker.add_result('addCustFunding', out)
 
     # customer initates unilateral closure using 'custClose' entrypoint. 
     # The merchant's closing signature is included in 'cust_close_json'.
@@ -195,12 +200,12 @@ def scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     feetracker.add_result('custClaim', out)
 
 
-def scenario2(feetracker, cust_py, merch_py, cust_close_json, establish_params):
+def test_dispute(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     '''
-    Scenario 2: First the merchant initiates a unilateral closure, then the customer closes on an outdated state and is punished by the merchant.
-    Entrypoints tested: 'addFunding', 'expiry', 'custClose', 'merchDispute'.
+    First the merchant initiates a unilateral closure, then the customer closes on an outdated state and is punished by the merchant.
+    Entrypoints tested: 'addCustFunding', 'expiry', 'custClose', 'merchDispute'.
     '''
-    print_header("Scenario 2: origination -> cust_funding -> merch_funding -> expiry -> cust_close -> merch_dispute")
+    print_header("Scenario test_dispute: origination -> cust_funding -> merch_funding -> expiry -> cust_close -> merch_dispute")
     # A single-funded channel is originated where the customer's initial balances (in mutez) is:
     cust_funding=establish_json.get("customer_deposit")
     merch_funding=establish_json.get("merchant_deposit")
@@ -212,7 +217,7 @@ def scenario2(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     merch_ci = merch_py.contract(contract_id)
 
     # customer funds their side of the contract with the same amount as their initial balance
-    out = add_funding(cust_ci, cust_funding)
+    out = add_cust_funding(cust_ci, cust_funding)
 
     # Merchant initiates closure by calling the 'expiry' entrypoint.
     out = entrypoint_no_args(merch_ci, 'expiry')
@@ -229,15 +234,40 @@ def scenario2(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     # out = merch_dispute(merch_ci, 'merchDispute', rev_secret)
     # feetracker.add_result('merchDispute', out)
 
-def scenario3(feetracker, cust_py, merch_py, cust_close_json, establish_params):
+def test_merchClaim(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     '''
-    Scenario 3: Tests the 'reclaimFunding' and 'merchClaim' entrypoints.
-    'reclaimFunding' is to be used in a dual funded channel where only one party has deposited their funds and wishes to abort channel establishment.
-    'expiry' is used by the merchant to force the customer to close the channel within the delay period.
+    Tests the 'merchClaim' entrypoint.
     'merchClaim' is used by the merchant to claim the total channel balance if the customer fails to respond to the 'expiry' call.
-    Entrypoints tested: 'addFunding', 'reclaimFunding', 'expiry', 'merchClaim'.
+    Entrypoints tested: 'addCustFunding', 'reclaimFunding'.
     '''
-    print_header("Scenario 3: origination -> cust_funding -> reclaim_funding -> cust_funding -> merch_funding -> expiry -> merch_claim")
+    print_header("Scenario test_merchClaim: origination -> cust_funding -> reclaim_funding")
+   
+    # A single-funded channel is originated where the customer's initial balances (in mutez) is:
+    cust_funding=establish_json.get("customer_deposit")
+    merch_funding=establish_json.get("merchant_deposit")
+
+    out, contract_id = originate(cust_py, establish_params, cust_funding, merch_funding)
+
+    # Set contract interfaces for cust and merch
+    cust_ci = cust_py.contract(contract_id)
+    merch_ci = merch_py.contract(contract_id)
+
+    # add customer's balance to the contract using 'addCustFunding' entrypoint
+    out = add_cust_funding(cust_ci, cust_funding)
+    feetracker.add_result('addCustFunding', out)
+
+    # Merchant initiates closure by calling the 'expiry' entrypoint.
+    out = entrypoint_no_args(merch_ci, 'expiry')
+
+    # If the customer has not broadcasted 'custClose' in response to 'expiry', the merchant can claim the full channel balance with 'merchClaim'.
+    out = entrypoint_no_args(merch_ci, 'merchClaim')
+    feetracker.add_result('merchClaim', out)
+
+def test_dualfund(feetracker, cust_py, merch_py, cust_close_json, establish_params):
+    '''
+    Tests the 'addCustFunding' and 'addMerchFunding' entrypoints.
+    '''
+    print_header("Scenario test_dualfund: origination -> cust_funding -> merch_funding -> reclaim_funding")
 
     # Create a dual-funded channel. If the establish_json is for a single funded channel, use the customer_deposit for the merchant so that it's non-zeo
     cust_funding=establish_json.get("customer_deposit")
@@ -253,21 +283,39 @@ def scenario3(feetracker, cust_py, merch_py, cust_close_json, establish_params):
     merch_ci = merch_py.contract(contract_id)
 
     # customer funds their side of the contract with the same amount as their initial balance
-    out = add_funding(cust_ci, cust_funding)
+    out = add_cust_funding(cust_ci, cust_funding)
+
+    # merchant funds their side of the contract with the same amount as their initial balance
+    out = add_merch_funding(merch_ci, merch_funding)
+    feetracker.add_result('addMerchFunding', out)
+
+def test_reclaim(feetracker, cust_py, merch_py, cust_close_json, establish_params):
+    '''
+    Tests the 'reclaimFunding' entrypoint.
+    'reclaimFunding' is to be used in a dual funded channel where only one party has deposited their funds and wishes to abort channel establishment.
+    'expiry' is used by the merchant to force the customer to close the channel within the delay period.
+    Entrypoints tested: 'addCustFunding', 'reclaimFunding'.
+    '''
+    print_header("Scenario test_reclaim: origination -> cust_funding -> reclaim_funding")
+
+    # Create a dual-funded channel. If the establish_json is for a single funded channel, use the customer_deposit for the merchant so that it's non-zeo
+    cust_funding=establish_json.get("customer_deposit")
+    if establish_json.get("merchant_deposit") == 0:
+        merch_funding=1000000
+    else:
+        merch_funding=establish_json.get("merchant_deposit")
+
+    out, contract_id = originate(cust_py, establish_params, cust_funding, merch_funding)
+    
+    # Set contract interfaces for cust
+    cust_ci = cust_py.contract(contract_id)
+
+    # customer funds their side of the contract with the same amount as their initial balance
+    out = add_cust_funding(cust_ci, cust_funding)
 
     # customer reclaims funding
     out = entrypoint_no_args(cust_ci, 'reclaimFunding')
     feetracker.add_result('reclaimFunding', out)
-
-    out = add_funding(cust_ci, cust_funding)
-    out = add_funding(merch_ci, merch_funding)
-
-    out = entrypoint_no_args(merch_ci, 'expiry')
-    feetracker.add_result('expiry', out)
-
-    # If the customer has not broadcasted 'custClose' in response to 'expiry', the merchant can claim the full channel balance with 'merchClaim'.
-    out = entrypoint_no_args(merch_ci, 'merchClaim')
-    feetracker.add_result('merchClaim', out)
 
 
 if __name__ == "__main__":
@@ -332,9 +380,11 @@ if __name__ == "__main__":
     merch_pubkey = merch_py.key.public_key()
 
     feetracker = FeeTracker()
-    scenario1(feetracker, cust_py, merch_py, cust_close_json, establish_json)
-    scenario2(feetracker, cust_py, merch_py, cust_close_json, establish_json)
-    scenario3(feetracker, cust_py, merch_py, cust_close_json, establish_json)
+    test_custclaim(feetracker, cust_py, merch_py, cust_close_json, establish_json)
+    test_dispute(feetracker, cust_py, merch_py, cust_close_json, establish_json)
+    test_merchClaim(feetracker, cust_py, merch_py, cust_close_json, establish_json)
+    test_dualfund(feetracker, cust_py, merch_py, cust_close_json, establish_json)
+    test_reclaim(feetracker, cust_py, merch_py, cust_close_json, establish_json)
     feetracker.print_fees()
 
     print("Tests finished!")
